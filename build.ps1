1364a99312171b1a6f849329b383ae66ec89bde6
Nico-Curti <nico.curti2@unibo.it>
2018-12-30 00:27:41 +0100

    remove cygwin

--- build.ps1
+++ build.ps1
@@ -9,7 +9,6 @@ Remove-Item build -Force -Recurse -ErrorAction SilentlyContinue
 New-Item -Path .\build -ItemType directory -Force > $null
 Set-Location build
 
-# cmake -T"LLVM-vs2015" -G "Visual Studio 15" "-DCMAKE_BUILD_TYPE=Release" ..
 cmake -G "Ninja" "-DCMAKE_BUILD_TYPE=Release" ..
-cmake --build . #--config Release
+cmake --build . --config Release
 Set-Location ..
