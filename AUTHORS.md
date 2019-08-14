f89a1b32e65b5d0674c5d7674147c6c00879ca76
Nico-Curti <nico.curti2@unibo.it>
2019-08-14 13:37:37 +0200

    minor updates

---
+++ AUTHORS.md
@@ -0,0 +1,4 @@
+# Authors #
+
+----------
+- Nico Curti - Department of Physics and Astronomy, University of Bologna ([nico.curti2@unibo.it](mailto:nico.curti2@unibo.it))
--- scorer/example/test.py
+++ scorer/example/test.py
@@ -5,7 +5,7 @@ from __future__ import division
 from __future__ import print_function
 
 #import numpy as np
-from scorer.Scorer import Scorer
+from scorer import Scorer
 
 __package__ = 'Scorer example test'
 __author__  = ['Nico Curti']
