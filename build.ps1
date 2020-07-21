4da3dc9c84181ceef21986301bb19e9e810503fd
Nico Curti <nico.curti2@unibo.it>
2020-07-21 12:30:17 +0200

    optional cython support and improvement in win compatibility

--- build.ps1
+++ build.ps1
@@ -2,31 +2,31 @@
 
 $number_of_build_workers=8
 
-if ($null -eq (Get-Command "cl.exe" -ErrorAction SilentlyContinue)) {
+if (Get-Command "cl.exe" -ErrorAction SilentlyContinue) {
   $vstype = "Professional"
-  if (Test-Path "C:\Program Files (x86)\Microsoft Visual Studio\2017\${vstype}\Common7\Tools") {
+  if (Test-Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\${vstype}\Common7\Tools") {
   }
   else {
     $vstype = "Enterprise"
-    if (Test-Path "C:\Program Files (x86)\Microsoft Visual Studio\2017\${vstype}\Common7\Tools") {
+    if (Test-Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\${vstype}\Common7\Tools") {
     }
     else {
       $vstype = "Community"
     }
   }
-  Write-Verbose "Found VS 2017 ${vstype}"
-  Push-Location "C:\Program Files (x86)\Microsoft Visual Studio\2017\${vstype}\Common7\Tools"
-  cmd /c "VsDevCmd.bat -arch=x64 & set" |
+  Write-Host "Found VS 2019 ${vstype}" -ForeGroundColor Yellow
+  Push-Location "C:\Program Files (x86)\Microsoft Visual Studio\2019\${vstype}\Common7\Tools"
+  cmd.exe /c "VsDevCmd.bat -arch=x64 & set" |
     ForEach-Object {
     if ($_ -match "=") {
-      $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+      $v = $_.split("="); Set-Item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
     }
   }
   Pop-Location
-  Write-Verbose "Visual Studio 2017 ${vstype} Command Prompt variables set.`n" -ForegroundColor Yellow
+  Write-Host "Visual Studio 2019 ${vstype} Command Prompt variables set.`n" -ForeGroundColor Yellow
 }
 else {
-  Write-Verbose "No Compiler found"
+  Write-Host "No Compiler found" -ForeGroundColor Red
 }
 
 
@@ -34,7 +34,7 @@ else {
 Remove-Item .\build_win_debug -Force -Recurse -ErrorAction SilentlyContinue
 New-Item -Path .\build_win_debug -ItemType directory -Force
 Set-Location build_win_debug
-cmake -G "Visual Studio 15 2017" -T "host=x64" -A "x64" "-DCMAKE_BUILD_TYPE=Debug" ..
+cmake -G "Visual Studio 16 2019" -T "host=x64" -A "x64" "-DCMAKE_BUILD_TYPE=Debug" "-DOMP=OFF" "-DPYwrap=OFF" ..
 cmake --build . --config Debug --parallel ${number_of_build_workers} --target install
 Set-Location ..
 
@@ -42,7 +42,7 @@ Set-Location ..
 Remove-Item .\build_win_release -Force -Recurse -ErrorAction SilentlyContinue
 New-Item -Path .\build_win_release -ItemType directory -Force
 Set-Location build_win_release
-cmake -G "Visual Studio 15 2017" -T "host=x64" -A "x64" "-DCMAKE_BUILD_TYPE=Release" ..
+cmake -G "Visual Studio 16 2019" -T "host=x64" -A "x64" "-DCMAKE_BUILD_TYPE=Release" "-DOMP=OFF" "-DPYwrap=ON" ..
 cmake --build . --config Release --parallel ${number_of_build_workers} --target install
 Set-Location ..
 
